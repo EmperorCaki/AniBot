b582b608000a4abce45ad5fbd3b2068f3ded0815
Hakea Dreaming <hakeadreaming@gmail.com>
2020-09-04 12:20:25 +1000

    Added functions for bot moderation.

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
 from json import dump, load, loads
+from pprint import pprint
 import requests
 
 
-def uwu_encode(plainText):
+def uwu_encode(plainText: str):
     encryptedText, ordText, binText = '', [], []
     for char in plainText:
         ordText.append(ord(char))
@@ -18,13 +19,13 @@ def uwu_encode(plainText):
     return encryptedText.strip()
 
 
-def uwu_decode(encryptedText):
+def uwu_decode(encryptedText: str):
     decryptedText, ordText, binText = '', [], []
-    for enchryptedChar in encryptedText.split('umu'):
+    for encryptedChar in encryptedText.split('umu'):
         binNumber = ''
-        if enchryptedChar == '':
+        if encryptedChar == '':
             break
-        for n in enchryptedChar.split():
+        for n in encryptedChar.split():
             if n == 'owo':
                 binNumber += '0'
             if n == 'uwu':
@@ -37,24 +38,78 @@ def uwu_decode(encryptedText):
     return decryptedText
 
 
-def link_accounts(discordAccountID, anilistAccount):
+def link_accounts(discordAccountID: str, anilistAccount: str):
     fileName = 'data/linked_accounts.json'
-    linkedAccounts = load(open(fileName))
+    linkedAccounts = load(open('data/linked_accounts.json'))
     print(linkedAccounts)
     if discordAccountID in linkedAccounts.keys():
         if linkedAccounts[discordAccountID] == anilistAccount:
             return f'Your account is already linked to {linkedAccounts[discordAccountID]}.'
         else:
             linkedAccounts[discordAccountID] = anilistAccount
-            dump(linkedAccounts, open(fileName, 'w'))
+            dump(linkedAccounts, open(fileName, 'w'), indent=4)
             return f'Your account has successfully been updated and is now linked to {linkedAccounts[discordAccountID]}'
     else:
         linkedAccounts[discordAccountID] = anilistAccount
-        dump(linkedAccounts, open(fileName, 'w'))
+        dump(linkedAccounts, open(fileName, 'w'), indent=4)
         return f'Your account has successfully been linked to {linkedAccounts[discordAccountID]}'
 
 
-def make_post_request_to_anilist_API(query, variables):
+def make_post_request_to_anilist_API(query: str, variables: dict):
     # Make the HTTPS Api request
     response = requests.post('https://graphql.anilist.co', json={'query': query, 'variables': variables})
     return loads(response.text), response.status_code
+
+
+def is_bot_admin(userID: int):
+    if userID in load(open('data/bot_config.json'))['AniAdmins']:
+        return True
+    else:
+        return False
+
+
+def get_image_url(image: str):
+    return load(open('data/bot_config.json'))['URLs'][image]
+
+
+# Here we define our query as a multi-line string
+Query = """
+query ($id: Int, $page: Int, $search: String, $type: MediaType) {
+    Page (page: $page, perPage: 10) {
+        media (id: $id, search: $search, type: $type) {
+            id
+            idMal
+            description(asHtml: false)
+            title {
+                english
+                romaji
+            }
+            coverImage {
+                medium
+            }
+            bannerImage
+            averageScore
+            meanScore
+            status
+            episodes
+            chapters
+            externalLinks {
+                url
+                site
+            }
+            nextAiringEpisode {
+                timeUntilAiring
+            }
+        }
+    }
+}
+"""
+
+# Define our query variables and values that will be used in the query request
+Variables = {
+    'id': 20665
+}
+
+# response, status = make_post_request_to_anilist_API(query, variables)
+# print(status)
+# pprint(response["data"])
